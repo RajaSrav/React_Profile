c2306090121b4cd7aaada5aadd2da7729ab8785d
Raja <gkrajareddy@gmail.com>
2020-04-21 21:25:00 +0530

    Updates

---
+++ precache-manifest.0c05f52063cab26f7849672034c6ca11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e821d71fc19d6c7a48a0321fe35a174",
+    "url": "/react_profile/index.html"
+  },
+  {
+    "revision": "a7150d1c36de200157e5",
+    "url": "/react_profile/static/css/main.90d82498.chunk.css"
+  },
+  {
+    "revision": "1f5c995a848e1cd2c7cd",
+    "url": "/react_profile/static/js/2.d6d1a696.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_profile/static/js/2.d6d1a696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7150d1c36de200157e5",
+    "url": "/react_profile/static/js/main.2e60176c.chunk.js"
+  },
+  {
+    "revision": "78733728cc62e730e75a",
+    "url": "/react_profile/static/js/runtime-main.fe3bd483.js"
+  }
+]);
